eb8fcdddbb27f3ed43967296a5697c847974b772
Jason Savelli <savellijason@gmail.com>
2021-07-12 08:02:31 +0200

    feat: useMinHeight hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Grid, makeStyles } from "@material-ui/core";
+import { useMinHeight } from "common";
+import { Header } from "layout";
+import Background from "./background-dark.png";
 
-function App() {
+const useStyles = makeStyles<{}, { minHeight: number }>(() => ({
+  root: {
+    minHeight: ({ minHeight }) => minHeight,
+  },
+}));
+
+const App = () => {
+  /* Vars */
+
+  const minHeight = useMinHeight();
+  const classes = useStyles({ minHeight });
+
+  /* Render */
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container className={classes.root}>
+      <Grid item xs={12}>
+        <Header />
+      </Grid>
+    </Grid>
   );
-}
+};
 
 export default App;
