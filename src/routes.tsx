47f65567255adfbac3363573e2d2df022276ad3f
Jason Savelli <savellijason@gmail.com>
2021-07-22 08:44:19 +0200

    feat: using suspense to display routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,19 @@
-import { Home } from "home";
+import { HomeSkeleton } from "home/home-skeleton";
+import { lazy, Suspense } from "react";
 import { Route, Switch } from "react-router-dom";
 
+// React.Lazy doesn't support named export yet.
+// https://fr.reactjs.org/docs/code-splitting.html#named-exports
+const Home = lazy(() => import("./home"));
+
 export const Routes = () => {
+  /* Render */
+
   return (
     <Switch>
-      <Route path="/" exact component={Home} />
+      <Suspense fallback={<HomeSkeleton />}>
+        <Route path="/" exact component={Home} />
+      </Suspense>
     </Switch>
   );
 };
