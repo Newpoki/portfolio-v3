368ebb9251bb0ba5dfba87525fdb9424db281aa0
Jason Savelli <savellijason@gmail.com>
2021-09-01 22:08:05 +0200

    task: add 404 route

--- src/i18n/fr-FR/common.ts
+++ src/i18n/fr-FR/common.ts
@@ -1,5 +1,13 @@
 export const frFRCommonTranslations = {
   COMMON: {
     TODAY: "Aujourd'hui",
+    PAGE: {
+      NOT_FOUND: {
+        TITLE: "Page introuvable",
+        BUTTON: {
+          TEXT: "Retour à l'accueil",
+        },
+      },
+    },
   },
 };
